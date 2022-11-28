0e2a86a527bfcab6bd304ce173453b54896cdaba
aajshaw <tony@adsaw.uk>
2022-11-28 15:04:25 +0000

    Adding first batch of files

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # LayoutControlLite
- Light weight, easy to use model railway layout control panel
+LayoutControlLite is a Python 3 library to enable schematic railway layouts to created with
+a mimimum of coding.
+
+The main aims of this project is to enable the deveoppment of layout control panels by people
+with little experience of writing programms and to run on inexpensive hardware.
+
+![turnout demo](simple_turnout.png)
+
+```python
+from LayoutControlLite import Track, Stub, Turnout, Signal
+
+# Define the individual items
+main = Track('Main Line', (25, 200), (1175, 200))
+turnout = Turnout('Turnout', (500, 200))
+siding = Stub('Siding', (600, 150), (800, 150))
+signal = Signal('Signal', (400, 225))
+
+# Create the layout and add the items
+layout = Layout('Simple Turnout')
+layout.add(main)
+layout.add(signal)
+layout.add(turnout)
+layout.add(siding)
+
+# Run the layout with all defaults
+layout.run()
+```
---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+setuptools.setup(
+    name="LayoutControlLite",
+    version="0.1.0",
+    author="Anthony Shaw",
+    author_email="tony@adshaw.uk",
+    description="Light weight, easy to use model railway layout control panel",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    keywords="model railway control panel lite",
+    url="https://github.com/aajshaw/LayoutControlLite",
+    packages=setuptools.find_packages(),
+    install_requires=['pysimplegui',],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: BSD 3-Clause :: BSD 3-Clause License",
+        "Topic :: SOFTWARE DEVELOPMENT :: LIBRARIES :: PYTHON_MODULES",
+        "Operating System :: OS Independent"
+    ),
+    zip_safe=True,
+)
