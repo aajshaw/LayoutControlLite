c91b30949490ee978f5627c540c1f4f6f058aa15
aajshaw <tony@adsaw.uk>
2022-11-28 16:13:56 +0000

    Fixed imports

--- __init__.py
+++ __init__.py
@@ -3,10 +3,10 @@ __package__ = 'ControlPanelLite'
 __version__ = '0.1.0'
 __author__ = 'Anthony Shaw'
 
-from .ControlPanelLite import Signal
-from .ControlPanelLite import Track
-from .ControlPanelLite import Stub
-from .ControlPanelLite import Turnout
-from .ControlPanelLite import Block
-from .ControlPanelLite import Route
-from .ControlPanelLite import Layout
+from .LayoutControlLite import Signal
+from .LayoutControlLite import Track
+from .LayoutControlLite import Stub
+from .LayoutControlLite import Turnout
+from .LayoutControlLite import Block
+from .LayoutControlLite import Route
+from .LayoutControlLite import Layout
